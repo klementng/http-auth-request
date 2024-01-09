3b908aad9be80147a83313e299dc3e1d41a0fac4
klementng <clement.ng.62@outlook.com>
2024-01-09 22:35:04 +0800

    change default values

--- server/config.py
+++ server/config.py
@@ -18,6 +18,6 @@ logging.basicConfig(
 os.environ["FLASK_SESSION_COOKIE_DOMAIN"] = os.getenv("FLASK_SESSION_COOKIE_DOMAIN", "")
 os.environ["FLASK_SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY", secrets.token_hex(16))
 os.environ["FLASK_SESSION_TYPE"] = os.getenv("FLASK_SESSION_TYPE", 'filesystem')
-os.environ["FLASK_SESSION_PERMANENT"] = os.getenv("FLASK_SESSION_PERMANENT", 'True')
+os.environ["FLASK_SESSION_PERMANENT"] = os.getenv("FLASK_SESSION_PERMANENT", 'False')
 
 
